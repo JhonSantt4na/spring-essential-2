f8db96f46104c51c40f52b97127716ce68333e92
Santt4naDev <jhonnsantt4na@gmail.com>
2025-02-11 19:26:14 -0300

    @Transactional

--- spring-essential-dev/src/main/java/com/santt4na/services/AnimeService.java
+++ spring-essential-dev/src/main/java/com/santt4na/services/AnimeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.santt4na.domain.Anime;
@@ -27,11 +28,25 @@ public class AnimeService {
 
    public Anime findByIdOrThrowBadRequestException(long id) {
       return animeRepository.findById(id)
-            .orElseThrow(() -> new BadRequestException( "Anime not found"));
+            .orElseThrow(() -> new BadRequestException("Anime not found"));
    }
 
+   // First look if in Data base, Engine = InnoDB
+
+   /*
+    * Por Padrão o @Trasactional ele so retorna o roolback
+    * para exceptions do tipo RuntimeException
+    * Para mudar usamos o (rollbackFor = Exception.class)
+    */
+   @Transactional // when we add this anotation, the Spring It only ends if everything goes well.
    public Anime save(AnimePostRequestBody animePostRequestBody) {
-      return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
+      // Simulating an exception
+      Anime save = animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
+      if (true) {
+         // even with an error the Database saves the object
+         throw new RuntimeException("Bad Code !");
+      }
+      return save;
    }
 
    public void delete(long id) {
